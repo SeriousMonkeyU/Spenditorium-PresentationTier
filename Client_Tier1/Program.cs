57407fa4100dfb607ccfc7035b12978bbfc82d4d
SeriousMonkeyU <berabbitshy@gmail.com>
2022-11-29 01:27:22 +0100

    Work start on viewBills

--- Client_Tier1/Program.cs
+++ Client_Tier1/Program.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Client_Tier1;
+using Syncfusion.Blazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new System.Net.Http.HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NzY5ODgzQDMyMzAyZTMzMmUzMGpWTC9EWEpDZGVMVHlvNGVZYjM1Y3pMT0hhYk10V2VnK2xiZU1rdXZGSmc9");
+builder.Services.AddSyncfusionBlazor();
 
 await builder.Build().RunAsync();
