6952efad1b383b0a6647aaf3d0c1148c1a86e3fc
SeriousMonkeyU <berabbitshy@gmail.com>
2022-12-16 05:15:11 +0100

    Final commit, minimum changes to css (error and success labels) + code cleaning and adding minimalistic notes and descriptions of some methods.

--- Client_Tier1/Service/IClientService.cs
+++ Client_Tier1/Service/IClientService.cs
@@ -6,6 +6,6 @@ namespace Client_Tier1.Service;
 public interface IClientService
 {
     public Task<List<Bill>> GetBillsById(int id, string company);
-    public Task RegisterAsync(ClientCreation client);
+    public Task<bool> RegisterAsync(ClientCreation client);
     public Task<int> LoginAsync(string username, string password);
 }
